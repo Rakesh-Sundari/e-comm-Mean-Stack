baea9a15e8836fb1683edf48682cce8e9127eafd
Rakesh Sundari <rakesh08sundari@gmail.com>
2025-09-02 19:38:09 -0400

    Deploy cancel order functionality - customer and admin cancel features

--- webapp/src/environments/environment.ts
+++ webapp/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://e-comm-mean-stack.onrender.com',
-  frontendUrl: 'https://saarvi.vercel.app',
+  production: false,
+  apiUrl: 'http://localhost:3000',
+  frontendUrl: 'http://localhost:4200',
   stripePublishableKey: 'pk_test_51S2M7pBiOdK8gYs8ynQUFp3zDcwxtTFeeFJguXFwiTJixcEBSS7O7hccLXzNeSQnelTQL2YOS4TfrJgkj3XWQadi0015BKbYKr'
 };
