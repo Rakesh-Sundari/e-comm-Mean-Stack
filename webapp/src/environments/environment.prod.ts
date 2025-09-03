2e4784ef0058ca7141901159060431a91cd810f6
Rakesh Sundari <rakesh08sundari@gmail.com>
2025-09-03 00:15:54 -0400

    SIMPLIFY: Use single environment file with auto-detection - no more localhost issues

--- webapp/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://e-comm-mean-stack.onrender.com',
-  frontendUrl: 'https://saarvi.vercel.app',
-  stripePublishableKey: 'pk_test_51S2M7pBiOdK8gYs8ynQUFp3zDcwxtTFeeFJguXFwiTJixcEBSS7O7hccLXzNeSQnelTQL2YOS4TfrJgkj3XWQadi0015BKbYKr'
-};
