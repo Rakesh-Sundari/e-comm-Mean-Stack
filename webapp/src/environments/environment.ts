3ac22802e5186a8f3ad662d8b40c9999ad03cfc5
Rakesh Sundari <rakesh08sundari@gmail.com>
2025-09-02 23:54:28 -0400

    env

--- webapp/src/environments/environment.ts
+++ webapp/src/environments/environment.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
-  frontendUrl: 'http://localhost:4200',
+  apiUrl: 'https://e-comm-mean-stack.onrender.com',
+  frontendUrl: 'https://saarvi.vercel.app',
+  //apiUrl: 'http://localhost:3000',
+  //frontendUrl: 'http://localhost:4200',
   stripePublishableKey: 'pk_test_51S2M7pBiOdK8gYs8ynQUFp3zDcwxtTFeeFJguXFwiTJixcEBSS7O7hccLXzNeSQnelTQL2YOS4TfrJgkj3XWQadi0015BKbYKr'
 };
