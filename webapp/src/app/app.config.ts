4696eca6f56ed3d85b9aa8ca1bdecd7e3e0b9707
Rakesh Sundari <rakesh08sundari@gmail.com>
2025-09-03 01:37:40 -0400

    update

--- webapp/src/app/app.config.ts
+++ webapp/src/app/app.config.ts
@@ -1,10 +1,8 @@
 
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
-import { CarouselModule } from 'ngx-owl-carousel-o';
-
 import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenHttpInterceptor } from './core/token-http-interceptor';
 
@@ -14,7 +12,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes, withInMemoryScrolling({
       scrollPositionRestoration: 'top'
     })),
-    provideAnimationsAsync(),
+  provideAnimations(),
     provideHttpClient(withInterceptors([tokenHttpInterceptor])),
   ],
 };
